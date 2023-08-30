7f1414aae39ef68c722fec06b468d92585b4be3a
TaePoong719 <just5538@gmail.com>
2023-08-30 14:09:25 +0900

    Feat: Typescript 적용

--- api/movie.ts
+++ api/movie.ts
@@ -1,11 +1,12 @@
 import fetch from 'node-fetch'
+import { VercelRequest, VercelResponse } from '@vercel/node'
 
 const { APIKEY } = process.env
 
 // 노드환경에서 작동함
-export default async function handler (request, response) {
+export default async function handler (request:VercelRequest, response:VercelResponse) {
   const { title, page, id } = JSON.parse(request.body)
-  const url = id 
+  const url = id
     ? `https://omdbapi.com?apikey=${APIKEY}&i=${id}&plot=full` 
     : `https://omdbapi.com?apikey=${APIKEY}&s=${title}&page=${page}`
   const res = await fetch(url)
@@ -17,3 +18,6 @@ export default async function handler (request, response) {
 
 // 서버리스 함수란 컴퓨팅 서버를 직접 구축이나 관리하지 않고 그 기능을 함수 단위로 작성해서 바로 사용할 수 있는 하나의 기능이자 서비스
 // 네트워크 상태코드 : 200 이면 정상적으로 호출 된 경우 의미
+
+// Vercel 서비스가 지원해주는 Type 을 불러와서 사용
+// 보통 대부분 TS의 Interface를 제공해주니, DOCS나 NPM에서 지원되는지 확인하고 가져오면 된다.
